4cdeccfd92bc84a9ac7879101eae1e5e27053566
Falaleev Danil <danilkafalaleev@gmail.com>
2024-11-01 00:13:14 +0300

    render

--- main.py
+++ main.py
@@ -63,5 +63,5 @@ async def read_item():
     return {"results": reviews}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
